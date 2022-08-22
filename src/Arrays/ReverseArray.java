94d9e4bc0bb0f978d0015589e86024a41d5025ca
Yugal Batra <yugal.batra@rsllearning.com>
2022-08-22 15:26:10 +0530

    arranged

---
+++ src/Arrays/ReverseArray.java
@@ -0,0 +1,12 @@
+package Arrays;
+
+public class ReverseArray {
+
+    public static void reverse(int[] arr) {
+        for (int i = 0, j = arr.length - 1; i < j; i++, j--) {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+    }
+}
