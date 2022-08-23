393b023b2b25ff96c668e7139407f6b3fe1cd8f1
Yugal Batra <yugal.batra@rsllearning.com>
2022-08-24 01:46:00 +0530

    addedSearchInSortedArray

--- src/Arrays/ChocolateDistributionProblem.java
+++ src/Arrays/ChocolateDistributionProblem.java
@@ -5,8 +5,9 @@ import java.util.Arrays;
 public class ChocolateDistributionProblem {
 
     public static int minimumDifference(int[] arr, int packets) {
-        if (packets>arr.length) return -1;
+        if (packets > arr.length) return -1;
         Arrays.sort(arr);
+
         int minDiff = Integer.MAX_VALUE;
         for (int i = 0; i <= arr.length - packets; i++) {
             int diff = arr[packets - 1 + i] - arr[i];
---
+++ src/Arrays/SearchRotatedSortedArray.java
@@ -0,0 +1,26 @@
+package Arrays;
+
+public class SearchRotatedSortedArray {
+
+    public static int searchInRotatedSortedArray(int[] arr, int element) {
+        int start = 0, end = arr.length - 1;
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            if (arr[mid] == element) return mid;
+            if (arr[start] <= arr[mid]) {
+                if (arr[mid] > element && element >= arr[start]) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            } else {
+                if (arr[mid] < element && element <= arr[end]) {
+                    start = mid + 1;
+                } else {
+                    end = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+}
