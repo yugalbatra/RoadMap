1ecedefe5d92b33c80731173e589d1f12874f5f4
Yugal Batra <yugal.batra@rsllearning.com>
2022-09-02 23:43:32 +0530

    xyz

---
+++ src/Strings/checkAnagram.java
@@ -0,0 +1,15 @@
+package Strings;
+
+public class checkAnagram {
+
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) return false;
+        int[] count = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            count[s.charAt(i) - 'a']++;
+            count[t.charAt(i) - 'a']--;
+        }
+        for (int i : count) if (i != 0) return false;
+        return true;
+    }
+}
