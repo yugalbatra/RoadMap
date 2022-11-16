e36bf4204012500395db8ce2c8a6af5358b89e53
Yugal Batra <yugal.batra@rsllearning.com>
2022-11-17 01:07:56 +0530

    xyz

---
+++ src/BinaryTrees/InvertTree.java
@@ -0,0 +1,14 @@
+package BinaryTrees;
+
+public class InvertTree {
+
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) return null;
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+        invertTree(root.left);
+        invertTree(root.right);
+        return root;
+    }
+}
