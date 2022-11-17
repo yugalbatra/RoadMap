c87f404af4f7b7bdf4737fcf3051bf8d10705994
Yugal Batra <yugal.batra@rsllearning.com>
2022-11-18 02:38:15 +0530

    xyz

---
+++ src/BitManipulation/PowerOfTwo.java
@@ -0,0 +1,9 @@
+package BitManipulation;
+
+public class PowerOfTwo {
+
+    public static boolean isPowerOfTwo(long n) {
+        if (n <= 0) return false;
+        return (n & n - 1) == 0;
+    }
+}
