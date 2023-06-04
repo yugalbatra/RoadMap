411a91f304a7c7585976616f362680f763dda066
Yugal Batra <yugal.batra@rsllearning.com>
2023-06-04 16:58:54 +0530

    xyz

--- src/Arrays/MinimizeTheHeights.java
+++ src/Arrays/MinimizeTheHeights.java
@@ -1,8 +1,20 @@
 package Arrays;
 
+import java.util.Arrays;
+
 public class MinimizeTheHeights {
 
     int getMinDiff(int[] arr, int n, int k) {
-
+        Arrays.sort(arr);
+        int ans = arr[n - 1] - arr[0];
+        int max = arr[n - 1] - k;
+        int min = arr[0] + k;
+        for (int i = 0; i < n - 1; i++) {
+            int mx = Math.max(max, arr[i] + k);
+            int mn = Math.min(min, arr[i + 1] - k);
+            if(mn<0) continue;
+            ans = Math.min(mx - mn, ans);
+        }
+        return ans;
     }
 }
