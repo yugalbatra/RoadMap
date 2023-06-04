0f36a19a960903ef97273f81efe1cf8f7b42c93e
Yugal Batra <yugal.batra@rsllearning.com>
2023-06-04 13:20:09 +0530

    xyz

--- src/Arrays/MaxSubArraySum.java
+++ src/Arrays/MaxSubArraySum.java
@@ -2,8 +2,18 @@ package Arrays;
 
 public class MaxSubArraySum {
 
+    long maxSubarraySum(int[] arr, int n) {
+        long maxSum = arr[0], sum = arr[0];
+        for (int i = 1; i < n; i++) {
+            sum = Math.max(sum + arr[i], arr[i]);
+            maxSum = Math.max(sum, maxSum);
+        }
+        return maxSum;
+    }
+
     /**
      * Used Kadane's Algorithm to solve this question.
+     *
      * @param nums take input as array of integers.
      * @return return maxSubArraySum in integer.
      */
